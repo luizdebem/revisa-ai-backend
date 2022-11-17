adb17ea1e818f86ff2b6fe3497854bf7730dd155
luizdebem <luizdebem99@gmail.com>
2022-11-17 02:59:37 -0300

    Retrieve own quizzes

--- src/controllers/QuizController.js
+++ src/controllers/QuizController.js
@@ -1,4 +1,5 @@
 import QuizHelper from "../helpers/QuizHelper.js";
+import Answer from "../models/AnswerModel.js";
 import Quiz from "../models/QuizModel.js";
 
 const QuizController = {
@@ -9,6 +10,30 @@ const QuizController = {
       const quiz = new Quiz({ title, description, expiryDate, questions, password, teacherId });
       await quiz.save();
       res.status(200).json(quiz.toJSON());
+    } catch (error) {
+      res.status(500).json({ error: JSON.stringify(error) });
+    }
+  },
+  listByTeacher: async (req, res) => {
+    const { teacherId } = req.params;
+    try {
+      const quizzes = await Quiz.find({ teacherId });
+      let response = [];
+      for (const quiz of quizzes) {
+        const answerData = await Answer.aggregate([{ $match: { quizId: quiz._id } }, { $group: { _id: null, avgScore: { $avg: "$score" }, answerCount: { $sum: 1 } } }]);
+        response = [...response, { ...quiz.toJSON(), avgScore: answerData[0]?.avgScore || 0, answerCount: answerData[0]?.answerCount || 0 }];
+      }
+      return res.status(200).json(response);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error: JSON.stringify(error) });
+    }
+  },
+  listByPassword: async (req, res) => {
+    const { password } = req.params;
+    try {
+      const quiz = await Quiz.findOne({ password }, { "questions.correctAnswerIndex": 0 });
+      return res.status(200).json(quiz);
     } catch (error) {
       res.status(500).json({ error: JSON.stringify(error) });
     }
--- src/routes/QuizRouter.js
+++ src/routes/QuizRouter.js
@@ -5,5 +5,7 @@ import AuthMiddleware from "../middlewares/AuthMiddleware.js";
 const quizRouter = express.Router();
 
 quizRouter.post("/", AuthMiddleware, QuizController.create);
+quizRouter.get("/:password", QuizController.listByPassword);
+quizRouter.get("/teacher/:teacherId", QuizController.listByTeacher);
 
 export default quizRouter;
